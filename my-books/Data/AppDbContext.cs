e94ab879e4a9dcfe3d82a01befb1e58216e831b0
Susanta Kumar Bishoyi <susanta.bishoyi@SYMPHONYINDUSTRIAL.AI>
2022-10-08 20:17:01 +0530

    first commit

---
+++ my-books/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using my_books.Data.Models;
+
+namespace my_books_webapi.Data
+{
+    public class AppDbContext:DbContext
+    {
+        
+        public AppDbContext(DbContextOptions<DbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
